45f49ac1017a46030f0c8c472ef75011cc679e0b
Sarthak Gupta <sarthi.abhi@gmail.com>
2024-05-15 00:32:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,7 @@ import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-    """   Choose the fruits you want in your custom smoothie!"""
-)
+st.write("""   Choose the fruits you want in your custom smoothie!""")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be ", name_on_order)
